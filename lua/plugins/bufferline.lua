40d66b748bd1ee6c504249fd93d51ec77e4b4255
arc <arkadiuszglinianowicz@gmail.com>
2024-06-29 20:55:37 +0200

    fixed c++ format and diagnostics

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,27 +1,28 @@
 return {
-    "akinsho/bufferline.nvim",
-    lazy = false,
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        require("bufferline").setup({
-            options = {
-                diagnostics = "nvim_lsp",
-                diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                    if context.buffer:current() then
-                        return ""
-                    end
+	"akinsho/bufferline.nvim",
+	lazy = false,
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					if context.buffer:current() then
+						return ""
+					end
 
-                    -- return " "
-                    -- return ""
-                    -- return ""
-                    return ""
-                end,
-            },
-        })
-    end,
-    keys = {
-        { "<tab>",   "<cmd>bnext<cr>", desc = "Next buffer" },
-        { "<S-tab>", "<cmd>bprev<cr>", desc = "Prev buffer" },
-    },
+					-- return " "
+					-- return ""
+					-- return ""
+					return ""
+				end,
+			},
+		})
+	end,
+	keys = {
+		{ "<tab>", "<cmd>bnext<cr>", desc = "Next buffer" },
+		{ "<S-tab>", "<cmd>bprev<cr>", desc = "Prev buffer" },
+		{ "<leader>x", "<cmd>bdelete<cr>", desc = "Close buffer" },
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,14 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
+				-- null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.diagnostics.clazy,
+				null_ls.builtins.formatting.clang_format.with({
+					-- extra_args = { "-dump-config" },
+					-- extra_args = { "-style='{IndentWidth: 4}'" },
+                    -- extra_args = { "-style", "{IndentWidth: 10}", "-dump-config" },
+                    extra_args = { "-style", "{IndentWidth: 4}" },
+				}),
 			},
 		})
 
