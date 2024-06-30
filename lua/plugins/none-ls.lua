0ebbba78b2b771a528f427d2cc922568bd43511b
arc <arkadiuszglinianowicz@gmail.com>
2024-06-30 23:40:54 +0200

    modified: none-ls, neo-tree

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,17 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.diagnostics.clazy,
-				null_ls.builtins.formatting.clang_format.with({
-					-- extra_args = { "-dump-config" },
-					-- extra_args = { "-style='{IndentWidth: 4}'" },
-                    -- extra_args = { "-style", "{IndentWidth: 10}", "-dump-config" },
-                    extra_args = { "-style", "{IndentWidth: 4}" },
-				}),
-			},
-		})
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.clang_format.with({
+                    extra_args = { "-style", "{IndentWidth: 4, ObjCBlockIndentWidth: 4, TabWidth: 4,}", },
+                }),
+            },
+        })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
