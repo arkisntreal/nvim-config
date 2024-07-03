8ba40bbe1fb8cb1f90ee1e791f1f6167f9a12dee
arc <arkadiuszglinianowicz@gmail.com>
2024-07-03 15:51:28 +0200

    testing noice and changed theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,19 @@
 return {
-    -- "nyoom-engineering/oxocarbon.nvim",
-    "scottmckendry/cyberdream.nvim",
-    -- "olivercederborg/poimandres.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.opt.background = "dark"
-        -- vim.cmd("colorscheme oxocarbon")
-        vim.cmd("colorscheme cyberdream")
-        -- vim.cmd("colorscheme poimandres")
-    end
+	-- "nyoom-engineering/oxocarbon.nvim",
+	-- "scottmckendry/cyberdream.nvim",
+	-- "olivercederborg/poimandres.nvim",
+	-- "folke/tokyonight.nvim",
+    -- "rebelot/kanagawa.nvim",
+    "EdenEast/nightfox.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.opt.background = "dark"
+		-- vim.cmd("colorscheme oxocarbon")
+		-- vim.cmd("colorscheme cyberdream")
+		-- vim.cmd("colorscheme poimandres")
+		-- vim.cmd("colorscheme tokyonight")
+        -- vim.cmd("colorscheme kanagawa")
+        vim.cmd("colorscheme nightfox")
+	end,
 }
