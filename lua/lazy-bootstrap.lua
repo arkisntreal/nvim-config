ea83709bdf71be1867c319905269a20a85291ceb
Ark <85484878+arkisntreal@users.noreply.github.com>
2025-02-10 08:45:25 +0100

    Update lazy-bootstrap.lua
    was a slight issue where it just didnt work u kno

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -1,10 +1,13 @@
 --  Boostrapping
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.gir"
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 --  Setup
 require("lazy").setup("plugins")
